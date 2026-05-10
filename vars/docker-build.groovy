0758bbfc2c378f04635b1fad063967be762881a1
Pranit Dhekane <49141184+pranitdhekane@users.noreply.github.com>
2026-05-10 18:08:51 +0530

    Update docker-build function parameters and logic
    Refactor Docker build function to accept username and project name.

--- vars/docker-build.groovy
+++ vars/docker-build.groovy
@@ -1,12 +1,6 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
-    sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
-    """
+def call(String username, String projectname, String imagetag, String Dockerfileptath){
+    sh "Building Docker image"
+
+    sh "docker build -t  ${username}/${projectname}:${imagetag} \
+    -f ${Dockerfileptath}"
 }
