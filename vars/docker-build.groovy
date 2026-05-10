c45601cf3f6bd00a2b53b704ba281d5525c0b52d
Pranit Dhekane <49141184+pranitdhekane@users.noreply.github.com>
2026-05-10 18:16:27 +0530

    Correct Docker build command syntax
    Fixed Dockerfile path and added current directory to build context.

--- vars/docker-build.groovy
+++ vars/docker-build.groovy
@@ -1,6 +1,8 @@
 def call(String username, String projectname, String imagetag, String Dockerfileptath){
     sh "Building Docker image"
 
-    sh "docker build -t  ${username}/${projectname}:${imagetag} \
-    -f ${Dockerfileptath}"
+    sh """
+    docker build -t  ${username}/${projectname}:${imagetag} \
+    -f ${Dockerfileptath} .
+    """
 }
