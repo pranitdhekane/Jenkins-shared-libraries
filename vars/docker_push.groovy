7fa9dd84c9a1b6243eddf54ec238ff0174bda7af
Pranit Dhekane <49141184+pranitdhekane@users.noreply.github.com>
2026-05-10 19:05:01 +0530

    Fix docker push script and login command order

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,11 @@
 def call(String dockerhubuser, String projectname, String imagetag){
-    withCredentials([usernamePassword(credentialId:"dockerhubcred" , passwodvariable:"dockerhubPass", userVariable:"dockerhubUser")]){
-        sh "docker login -u ${dockerhubUser} -p ${dockerhubPass}"
+    withCredentials([usernamePassword(credentialsId:"dockerhubcred",
+                                      passwordVariable:"dockerhubPass",
+                                      usernameVariable:"dockerhubUser")]){    
     
-        
+    sh """
+    docker push ${dockerhubuser}/${projectname}:${imagetag}
+    docker login -u ${dockerhubUser} -p ${dockerhubPass}
+    """
     }
-    sh "docker push ${dockerhubuser}/${projectname}:${imagetag}"
 }
