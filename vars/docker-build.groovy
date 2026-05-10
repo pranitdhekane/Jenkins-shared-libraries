b1001d21b7e3c1c5e9d1aefed33852856118a52e
pranitdhekane <dpranit61@gmail.com>
2026-05-10 19:59:36 +0530

    Remove un used files

--- vars/docker-build.groovy
+++
@@ -1,8 +0,0 @@
-def call(String username, String projectname, String imagetag, String Dockerfileptath){
-    echo "Building Docker image"
-
-    sh """
-    docker build -t  ${username}/${projectname}:${imagetag} \
-    -f ${Dockerfileptath} .
-    """
-}
--- vars/docker-push.groovy
+++
@@ -1,8 +0,0 @@
-def call(String dockerhubuser, String projectname, String imagetag){
-    withCredentials([usernamePassword(credentialId:"dockerhubcred" , passwodvariable:"dockerhubPass", userVariable:"dockerhubUser")]){
-        sh "docker login -u ${dockerhubUser} -p ${dockerhubPass}"
-    
-        
-    }
-    sh "docker push ${dockerhubuser}/${projectname}:${imagetag}
-}
